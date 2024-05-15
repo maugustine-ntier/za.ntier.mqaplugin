990619a82efb96a43b7cc0f948be73fc1c72b84f
martin <martin@martin-VivoBook-ASUSLaptop-X512FB-X512FB>
2024-05-15 14:03:03 +0200

    Add edit checks for dup horses and trailers

--- src/za/ntier/models/MTruckList.java
+++ src/za/ntier/models/MTruckList.java
@@ -38,16 +38,46 @@ public class MTruckList extends X_ZZ_Truck_List implements I_ZZ_Truck_List {
 	protected boolean beforeSave(boolean newRecord) {
 		if (getZZ_Driver_ID() > 0) {
 			String SQL = "Select count(*) from ZZ_Truck_List tl where tl.ZZ_Transporters_ID = " + getZZ_Transporters_ID() 
-					+ " and tl." + this.COLUMNNAME_ZZ_Driver_ID + " = " + getZZ_Driver_ID() + " and tl.isActive = 'Y' and (" + getZZ_Truck_List_ID() + " <= 0 or tl.ZZ_Truck_List_ID <> " + getZZ_Truck_List_ID() + ")";
+			+ " and tl." + this.COLUMNNAME_ZZ_Driver_ID + " = " + getZZ_Driver_ID() + " and tl.isActive = 'Y' and (" + getZZ_Truck_List_ID() + " <= 0 or tl.ZZ_Truck_List_ID <> " + getZZ_Truck_List_ID() + ")";
 			if (DB.getSQLValue(get_TrxName(), SQL) > 0) {
-				log.saveError("Error", "Under this transport,Cannot have multiple truck lists with the same driver"); 
+				log.saveError("Error", "Under this transporter,Cannot have multiple truck lists with the same driver"); 
 				return false;
 			}
-			
+
 		}
 		if (getZZ_Trailer1_ID() > 0 && getZZ_Trailer2_ID() > 0 && getZZ_Trailer1_ID() == getZZ_Trailer2_ID()) {
 			log.saveError("Error", "Trailers 1 and 2 should be different"); 
 			return false;
+		}
+
+		if (getZZ_Horse_ID() > 0) {
+			String SQL = "Select count(*) from ZZ_Truck_List tl where tl.ZZ_Transporters_ID = " + getZZ_Transporters_ID() 
+			+ " and tl.ZZ_Horse_ID" + " = " + getZZ_Horse_ID()
+			+ " and tl.isActive = 'Y' and (" + getZZ_Truck_List_ID() + " <= 0 or tl.ZZ_Truck_List_ID <> " + getZZ_Truck_List_ID() + ")";
+			if (DB.getSQLValue(get_TrxName(), SQL) > 0) {
+				log.saveError("Error", "Under this transporter,this horse has already been listed"); 
+				return false;
+			}
+		}
+		
+		if (getZZ_Trailer1_ID() > 0) {
+			String SQL = "Select count(*) from ZZ_Truck_List tl where tl.ZZ_Transporters_ID = " + getZZ_Transporters_ID() 
+			+ " and (tl.ZZ_Trailer1_ID " + " = " + getZZ_Trailer1_ID() + " OR tl.ZZ_Trailer2_ID " + " = " + getZZ_Trailer1_ID() + ")"
+			+ " and tl.isActive = 'Y' and (" + getZZ_Truck_List_ID() + " <= 0 or tl.ZZ_Truck_List_ID <> " + getZZ_Truck_List_ID() + ")";
+			if (DB.getSQLValue(get_TrxName(), SQL) > 0) {
+				log.saveError("Error", "Under this transporter,Trailer 1 has already been listed"); 
+				return false;
+			}
+		}
+		
+		if (getZZ_Trailer2_ID() > 0) {
+			String SQL = "Select count(*) from ZZ_Truck_List tl where tl.ZZ_Transporters_ID = " + getZZ_Transporters_ID() 
+			+ " and (tl.ZZ_Trailer1_ID " + " = " + getZZ_Trailer2_ID() + " OR tl.ZZ_Trailer2_ID " + " = " + getZZ_Trailer2_ID() + ")"
+			+ " and tl.isActive = 'Y' and (" + getZZ_Truck_List_ID() + " <= 0 or tl.ZZ_Truck_List_ID <> " + getZZ_Truck_List_ID() + ")";
+			if (DB.getSQLValue(get_TrxName(), SQL) > 0) {
+				log.saveError("Error", "Under this transporter,Trailer 2 has already been listed"); 
+				return false;
+			}
 		}
 		return super.beforeSave(newRecord);
 	}
