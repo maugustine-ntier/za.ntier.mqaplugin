8735f103207e8394d831d26dbd2ab3e92b52a2c5
martin <maugustine@ntier.co.za>
2025-03-04 10:33:52 +0200

    Petty Cash advance completed

---
+++ src/org/compiere/model/I_ZZ_Petty_Cash_Advance_Line.java
@@ -0,0 +1,175 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for ZZ_Petty_Cash_Advance_Line
+ *  @author iDempiere (generated) 
+ *  @version Release 12
+ */
+public interface I_ZZ_Petty_Cash_Advance_Line 
+{
+
+    /** TableName=ZZ_Petty_Cash_Advance_Line */
+    public static final String Table_Name = "ZZ_Petty_Cash_Advance_Line";
+
+    /** AD_Table_ID=1000003 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 3 - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(3);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Tenant.
+	  * Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within tenant
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within tenant
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Amount */
+    public static final String COLUMNNAME_Amount = "Amount";
+
+	/** Set Amount.
+	  * Amount in a defined currency
+	  */
+	public void setAmount (BigDecimal Amount);
+
+	/** Get Amount.
+	  * Amount in a defined currency
+	  */
+	public BigDecimal getAmount();
+
+    /** Column name C_Charge_ID */
+    public static final String COLUMNNAME_C_Charge_ID = "C_Charge_ID";
+
+	/** Set Charge.
+	  * Additional document charges
+	  */
+	public void setC_Charge_ID (int C_Charge_ID);
+
+	/** Get Charge.
+	  * Additional document charges
+	  */
+	public int getC_Charge_ID();
+
+	public org.compiere.model.I_C_Charge getC_Charge() throws RuntimeException;
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+
+    /** Column name ZZ_Petty_Cash_Advance_Hdr_ID */
+    public static final String COLUMNNAME_ZZ_Petty_Cash_Advance_Hdr_ID = "ZZ_Petty_Cash_Advance_Hdr_ID";
+
+	/** Set Petty Cash Advance	  */
+	public void setZZ_Petty_Cash_Advance_Hdr_ID (int ZZ_Petty_Cash_Advance_Hdr_ID);
+
+	/** Get Petty Cash Advance	  */
+	public int getZZ_Petty_Cash_Advance_Hdr_ID();
+
+	public I_ZZ_Petty_Cash_Advance_Hdr getZZ_Petty_Cash_Advance_Hdr() throws RuntimeException;
+
+    /** Column name ZZ_Petty_Cash_Advance_Line_ID */
+    public static final String COLUMNNAME_ZZ_Petty_Cash_Advance_Line_ID = "ZZ_Petty_Cash_Advance_Line_ID";
+
+	/** Set Petty Cash Advance	  */
+	public void setZZ_Petty_Cash_Advance_Line_ID (int ZZ_Petty_Cash_Advance_Line_ID);
+
+	/** Get Petty Cash Advance	  */
+	public int getZZ_Petty_Cash_Advance_Line_ID();
+
+    /** Column name ZZ_Petty_Cash_Advance_Line_UU */
+    public static final String COLUMNNAME_ZZ_Petty_Cash_Advance_Line_UU = "ZZ_Petty_Cash_Advance_Line_UU";
+
+	/** Set ZZ_Petty_Cash_Advance_Line_UU	  */
+	public void setZZ_Petty_Cash_Advance_Line_UU (String ZZ_Petty_Cash_Advance_Line_UU);
+
+	/** Get ZZ_Petty_Cash_Advance_Line_UU	  */
+	public String getZZ_Petty_Cash_Advance_Line_UU();
+
+    /** Column name ZZ_Petty_Cash_Motivation */
+    public static final String COLUMNNAME_ZZ_Petty_Cash_Motivation = "ZZ_Petty_Cash_Motivation";
+
+	/** Set Motivation	  */
+	public void setZZ_Petty_Cash_Motivation (String ZZ_Petty_Cash_Motivation);
+
+	/** Get Motivation	  */
+	public String getZZ_Petty_Cash_Motivation();
+}
--- src/za/ntier/process/PettyCashAdvanceRequest.java
+++ src/za/ntier/process/PettyCashAdvanceRequest.java
@@ -12,14 +12,16 @@ import org.compiere.model.MProcessPara;
 import org.compiere.model.MSysConfig;
 import org.compiere.model.MUser;
 import org.compiere.model.MUserRoles;
+import org.compiere.model.PO;
 import org.compiere.model.X_AD_User;
+import org.compiere.model.X_ZZ_Petty_Cash_Advance_Hdr;
+import org.compiere.model.X_ZZ_Petty_Cash_Advance_Line;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.CLogger;
 import org.compiere.util.Env;
 
 import za.ntier.models.MZZPettyCashAdvanceHdr;
-import za.ntier.models.X_ZZ_Petty_Cash_Application;
 
 
 @org.adempiere.base.annotation.Process
@@ -59,29 +61,33 @@ public class PettyCashAdvanceRequest extends SvrProcess {
 	protected String doIt() throws Exception {
 		int zz_Petty_Cash_Advance_Hdr_ID = getRecord_ID();
 		MZZPettyCashAdvanceHdr mZZPettyCashAdvanceHdr = new MZZPettyCashAdvanceHdr(getCtx(),zz_Petty_Cash_Advance_Hdr_ID,get_TrxName());
-		if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(mZZPettyCashAdvanceHdr.ZZ_DOCACTION_SubmitToLineManager)) {
+		int x_ZZ_Petty_Cash_Advance_Line_IDs[] = PO.getAllIDs(X_ZZ_Petty_Cash_Advance_Line.Table_Name,MZZPettyCashAdvanceHdr.COLUMNNAME_ZZ_Petty_Cash_Advance_Hdr_ID + " = " + zz_Petty_Cash_Advance_Hdr_ID , null);
+		if (x_ZZ_Petty_Cash_Advance_Line_IDs == null || x_ZZ_Petty_Cash_Advance_Line_IDs.length <= 0) {
+			return "@Error@Please enter at least one Petty Cash Advance Line";
+		}
+		if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(MZZPettyCashAdvanceHdr.ZZ_DOCACTION_SubmitToLineManager)) {
 			if (mZZPettyCashAdvanceHdr.getLine_Manager_ID() <= 0) {
 				return "Please select a Line Manager";
 			}
-			mZZPettyCashAdvanceHdr.setZZ_DocStatus(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Submitted);
-			mZZPettyCashAdvanceHdr.setZZ_DocAction(mZZPettyCashAdvanceHdr.ZZ_DOCACTION_ApproveDoNotApprove);
+			mZZPettyCashAdvanceHdr.setZZ_DocStatus(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Submitted);
+			mZZPettyCashAdvanceHdr.setZZ_DocAction(MZZPettyCashAdvanceHdr.ZZ_DOCACTION_ApproveDoNotApprove);
 			mZZPettyCashAdvanceHdr.setZZ_Date_Submitted(new Timestamp(System.currentTimeMillis()));
 			String subject = PETTY_CASH_CARD_APPLICATION + "awaiting LM Approval";
 			String message = PLEASE_APPROVE_OR_REJECT_THE_PETTY_CASH_CARD_APPLICATION + mZZPettyCashAdvanceHdr.getDocumentNo();
 			int ad_Message_ID = MESSAGE_NEW_PETTYCASH_APP;
 			sendNotification(mZZPettyCashAdvanceHdr.getLine_Manager_ID(),zz_Petty_Cash_Advance_Hdr_ID,subject,message,ad_Message_ID);
-		} else if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(mZZPettyCashAdvanceHdr.ZZ_DOCACTION_ApproveDoNotApprove) && 
-				mZZPettyCashAdvanceHdr.getZZ_DocStatus().equals(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Submitted)) {
+		} else if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(MZZPettyCashAdvanceHdr.ZZ_DOCACTION_ApproveDoNotApprove) && 
+				mZZPettyCashAdvanceHdr.getZZ_DocStatus().equals(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Submitted)) {
 			if (p_ZZ_Approve_Rej_LM.equals("Y")) {
-				mZZPettyCashAdvanceHdr.setZZ_DocStatus(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_InProgress);
-				mZZPettyCashAdvanceHdr.setZZ_DocAction(mZZPettyCashAdvanceHdr.ZZ_DOCACTION_FinalApprovalDoNotApprove);
+				mZZPettyCashAdvanceHdr.setZZ_DocStatus(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_InProgress);
+				mZZPettyCashAdvanceHdr.setZZ_DocAction(MZZPettyCashAdvanceHdr.ZZ_DOCACTION_FinalApprovalDoNotApprove);
 				mZZPettyCashAdvanceHdr.setZZ_Date_LM_Approved(new Timestamp(System.currentTimeMillis()));
 				String subject = PETTY_CASH_CARD_APPLICATION + "awaiting Finance Approval";
 				String message = PLEASE_APPROVE_OR_REJECT_THE_PETTY_CASH_CARD_APPLICATION + mZZPettyCashAdvanceHdr.getDocumentNo();
 				int ad_Message_ID = MESSAGE_LM_APPROVED_PETTYCASH_APP;
 				sendNotificationToRole(SNR_ADMIN_FIN_ROLE_ID,zz_Petty_Cash_Advance_Hdr_ID,subject,message,ad_Message_ID);
 			} else {
-				mZZPettyCashAdvanceHdr.setZZ_DocStatus(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_NotApprovedByLM);
+				mZZPettyCashAdvanceHdr.setZZ_DocStatus(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_NotApprovedByLM);
 				mZZPettyCashAdvanceHdr.setZZ_Date_Not_Approved_by_LM(new Timestamp(System.currentTimeMillis()));
 				String subject = PETTY_CASH_CARD_APPLICATION + "Rejected by the LM";
 				String message = YOUR_APPLICATION_WAS_REJECTED_PETTY_CASH_CARD_APPLICATION + mZZPettyCashAdvanceHdr.getDocumentNo();
@@ -89,10 +95,10 @@ public class PettyCashAdvanceRequest extends SvrProcess {
 				sendNotification(mZZPettyCashAdvanceHdr.getCreatedBy(),zz_Petty_Cash_Advance_Hdr_ID,subject,message,ad_Message_ID);
 			}
 
-		} else if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(mZZPettyCashAdvanceHdr.ZZ_DOCACTION_FinalApprovalDoNotApprove) && 
-				mZZPettyCashAdvanceHdr.getZZ_DocStatus().equals(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_InProgress)) {
+		} else if (mZZPettyCashAdvanceHdr.getZZ_DocAction().equals(MZZPettyCashAdvanceHdr.ZZ_DOCACTION_FinalApprovalDoNotApprove) && 
+				mZZPettyCashAdvanceHdr.getZZ_DocStatus().equals(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_InProgress)) {
 			if (p_ZZ_Approve_Rej_SAF.equals("Y")) {
-				mZZPettyCashAdvanceHdr.setZZ_DocStatus(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Completed);
+				mZZPettyCashAdvanceHdr.setZZ_DocStatus(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_Completed);
 				mZZPettyCashAdvanceHdr.setZZ_Date_Completed(new Timestamp(System.currentTimeMillis()));
 				mZZPettyCashAdvanceHdr.setZZ_Snr_Admin_Fin_ID(Env.getAD_User_ID(getCtx()));
 				String subject = PETTY_CASH_CARD_APPLICATION + "Has been Approved by Finance";
@@ -100,7 +106,7 @@ public class PettyCashAdvanceRequest extends SvrProcess {
 				int ad_Message_ID = MESSAGE_NEW_PETTYCASH_APP;
 				sendNotification(mZZPettyCashAdvanceHdr.getCreatedBy(),zz_Petty_Cash_Advance_Hdr_ID,subject,message,ad_Message_ID);
 			} else {
-				mZZPettyCashAdvanceHdr.setZZ_DocStatus(mZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_NotApprovedBySnrAdminFinance);
+				mZZPettyCashAdvanceHdr.setZZ_DocStatus(MZZPettyCashAdvanceHdr.ZZ_DOCSTATUS_NotApprovedBySnrAdminFinance);
 				mZZPettyCashAdvanceHdr.setZZ_Date_Not_Approved_by_Snr_Adm_Fin(new Timestamp(System.currentTimeMillis()));
 				String subject = PETTY_CASH_CARD_APPLICATION + "Rejected by Finance";
 				String message = YOUR_APPLICATION_WAS_REJECTED_PETTY_CASH_CARD_APPLICATION2 + mZZPettyCashAdvanceHdr.getDocumentNo();
@@ -118,12 +124,12 @@ public class PettyCashAdvanceRequest extends SvrProcess {
 		return null;
 	}
 
-	private void sendNotification(int ad_User_ID,int zz_Petty_Cash_Application_ID,String subject,String message,int ad_Message_ID) {
+	private void sendNotification(int ad_User_ID,int zz_Petty_Cash_Advance_Hdr_ID,String subject,String message,int ad_Message_ID) {
 		MUser mUser = new MUser(getCtx(), ad_User_ID, null);
 		if (X_AD_User.NOTIFICATIONTYPE_Notice.equals(mUser.getNotificationType()) ||
 				X_AD_User.NOTIFICATIONTYPE_EMailPlusNotice.equals(mUser.getNotificationType())	) {
 			MNote note = new MNote(getCtx(), ad_Message_ID, ad_User_ID,
-					X_ZZ_Petty_Cash_Application.Table_ID, zz_Petty_Cash_Application_ID, 
+					X_ZZ_Petty_Cash_Advance_Hdr.Table_ID, zz_Petty_Cash_Advance_Hdr_ID, 
 					subject, message.toString(), get_TrxName());
 			note.saveEx();
 		}
@@ -137,11 +143,11 @@ public class PettyCashAdvanceRequest extends SvrProcess {
 		}
 	}
 
-	private void sendNotificationToRole(int ad_Role_ID,int zz_Petty_Cash_Application_ID,String subject,String message,int ad_Message_ID) {
+	private void sendNotificationToRole(int ad_Role_ID,int zz_Petty_Cash_Advance_Hdr_ID,String subject,String message,int ad_Message_ID) {
 		MUserRoles [] mUserRoles = MUserRoles.getOfRole(getCtx(), ad_Role_ID);
 		for (MUserRoles mUserRole : mUserRoles) {
 			if (mUserRole.isActive()) {
-				sendNotification(mUserRole.getAD_User_ID(),zz_Petty_Cash_Application_ID,subject,message,ad_Message_ID);
+				sendNotification(mUserRole.getAD_User_ID(),zz_Petty_Cash_Advance_Hdr_ID,subject,message,ad_Message_ID);
 			}
 		}
 	}
