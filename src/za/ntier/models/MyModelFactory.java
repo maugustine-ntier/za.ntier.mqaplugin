e8a4842a11550b486a714b2a75a1066c28488548
martin <maugustine@ntier.co.za>
2025-11-14 12:50:46 +0200

    refactor model factory

--- src/za/ntier/models/MyModelFactory.java
+++ src/za/ntier/models/MyModelFactory.java
@@ -1,13 +1,16 @@
 package za.ntier.models;
 
+import java.lang.reflect.Constructor;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import org.adempiere.base.IModelFactory;
-import org.compiere.model.I_C_BP_BankAccount;
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.PO;
 import org.compiere.util.Env;
 import org.osgi.service.component.annotations.Component;
-
 import za.co.ntier.api.model.I_ZZ_Program_Master_Data;
 import za.co.ntier.wf.model.MZZWFHeader;
 import za.co.ntier.wf.model.MZZWFLineRole;
@@ -20,201 +23,94 @@ import za.co.ntier.wf.model.MZZWFLines;
 		 )
 public class MyModelFactory implements IModelFactory {
 
-	@Override
-	public Class<?> getClass(String tableName) {
-		if (tableName.equals(I_ZZ_ATRVerification.Table_Name)) {
-			return MZZATRVerification.class;
-		}
-		if (tableName.equals(I_ZZ_WF_Line_Role.Table_Name)) {
-			return MZZWFLineRole.class;
-		}
-		if (tableName.equals(I_ZZ_WF_Lines.Table_Name)) {
-			return MZZWFLines.class;
-		}
-		if (tableName.equals(I_ZZ_WF_Header.Table_Name)) {
-			return MZZWFHeader.class;
-		}
-		if (tableName.equals(I_ZZ_Monthly_Levy_Files.Table_Name)) {
-			return X_ZZ_Monthly_Levy_Files.class;
-		}
-		if (tableName.equals(I_ZZ_Open_Application.Table_Name)) {
-			return MZZOpenApplication.class;
-		}
-		if (tableName.equals(I_ZZ_WSP_ATR_Approvals.Table_Name)) {
-			return X_ZZ_WSP_ATR_Approvals.class;
-		}
-		if (tableName.equals(I_ZZ_Program_Master_Data.Table_Name)) {
-			return MZZProgramMasterData.class;
-		}
-		if (tableName.equals(I_ZZ_System_Access_Application.Table_Name)) {
-			return MZZSystemAccessApplication.class;
-		}
-		if (tableName.equals(I_M_Inventory.Table_Name)) {
-			return MInventory_New.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Advance.Table_Name)) {
-			return MZZPettyCashReconAdvance.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Claim.Table_Name)) {
-			return MZZPettyCashReconClaim.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Hdr.Table_Name)) {
-			return MZZPettyCashReconHdr.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Line.Table_Name)) {
-			return MZZPettyCashClaimLine.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Hdr.Table_Name)) {
-			return MZZPettyCashClaimHdr.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Line.Table_Name)) {
-			return MZZPettyCashAdvanceLine.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Hdr.Table_Name)) {
-			return MZZPettyCashAdvanceHdr.class;
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Application.Table_Name)) {
-			return MZZPettyCashApplication.class;
-		}
-		if (tableName.equals(I_C_InvoiceBatch.Table_Name)) {
-			return MInvoiceBatch_New.class;
-		}
-		if (tableName.equals(I_C_BP_BankAccount.Table_Name)) {
-			return MBPBankAccount_New.class;
-		}
-		return null;
-	}
+    private static class ModelDef {
+        final Class<? extends PO> modelClass;
+        final Constructor<? extends PO> idCtor;
+        final Constructor<? extends PO> rsCtor;
 
-	@Override
-	public PO getPO(String tableName, int Record_ID, String trxName) {
-		if (tableName.equals(I_ZZ_ATRVerification.Table_Name)) {
-			return new MZZATRVerification(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Line_Role.Table_Name)) {
-			return new MZZATRVerification(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Lines.Table_Name)) {
-			return new MZZWFLines(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Header.Table_Name)) {
-			return new MZZWFHeader(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Monthly_Levy_Files.Table_Name)) {
-			return new X_ZZ_Monthly_Levy_Files(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Open_Application.Table_Name)) {
-			return new MZZOpenApplication(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_WSP_ATR_Approvals.Table_Name)) {
-			return new X_ZZ_WSP_ATR_Approvals(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Program_Master_Data.Table_Name)) {
-			return new MZZProgramMasterData(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_System_Access_Application.Table_Name)) {
-			return new MZZSystemAccessApplication(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_M_Inventory.Table_Name)) {
-			return new MInventory_New(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Advance.Table_Name)) {
-			return new MZZPettyCashReconAdvance(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Claim.Table_Name)) {
-			return new MZZPettyCashReconClaim(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Hdr.Table_Name)) {
-			return new MZZPettyCashReconHdr(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Line.Table_Name)) {
-			return new MZZPettyCashClaimLine(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Hdr.Table_Name)) {
-			return new MZZPettyCashClaimHdr(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Line.Table_Name)) {
-			return new MZZPettyCashAdvanceLine(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Hdr.Table_Name)) {
-			return new MZZPettyCashAdvanceHdr(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Application.Table_Name)) {
-			return new MZZPettyCashApplication(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_C_InvoiceBatch.Table_Name)) {
-			return new MInvoiceBatch_New(Env.getCtx(),Record_ID,trxName);
-		}
-		if (tableName.equals(I_C_BP_BankAccount.Table_Name)) {
-			return new MBPBankAccount_New(Env.getCtx(),Record_ID,trxName);
-		}
-		
-		return null;
-	}
+        ModelDef(Class<? extends PO> modelClass,
+                 Constructor<? extends PO> idCtor,
+                 Constructor<? extends PO> rsCtor) {
+            this.modelClass = modelClass;
+            this.idCtor = idCtor;
+            this.rsCtor = rsCtor;
+        }
+    }
 
-	@Override
-	public PO getPO(String tableName, ResultSet rs, String trxName) {
-		if (tableName.equals(I_ZZ_ATRVerification.Table_Name)) {
-			return new MZZATRVerification(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Line_Role.Table_Name)) {
-			return new MZZWFLineRole(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Lines.Table_Name)) {
-			return new MZZWFLines(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_WF_Header.Table_Name)) {
-			return new MZZWFHeader(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Monthly_Levy_Files.Table_Name)) {
-			return new X_ZZ_Monthly_Levy_Files(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Open_Application.Table_Name)) {
-			return new MZZOpenApplication(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_WSP_ATR_Approvals.Table_Name)) {
-			return new X_ZZ_WSP_ATR_Approvals(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Program_Master_Data.Table_Name)) {
-			return new MZZProgramMasterData(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_System_Access_Application.Table_Name)) {
-			return new MZZSystemAccessApplication(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_M_Inventory.Table_Name)) {
-			return new MInventory_New(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Advance.Table_Name)) {
-			return new MZZPettyCashReconAdvance(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Claim.Table_Name)) {
-			return new MZZPettyCashReconClaim(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Recon_Hdr.Table_Name)) {
-			return new MZZPettyCashReconHdr(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Line.Table_Name)) {
-			return new MZZPettyCashClaimLine(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Claim_Hdr.Table_Name)) {
-			return new MZZPettyCashClaimHdr(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Line.Table_Name)) {
-			return new MZZPettyCashAdvanceLine(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Advance_Hdr.Table_Name)) {
-			return new MZZPettyCashAdvanceHdr(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_ZZ_Petty_Cash_Application.Table_Name)) {
-			return new MZZPettyCashApplication(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_C_InvoiceBatch.Table_Name)) {
-			return new MInvoiceBatch_New(Env.getCtx(),rs,trxName);
-		}
-		if (tableName.equals(I_C_BP_BankAccount.Table_Name)) {
-			return new MBPBankAccount_New(Env.getCtx(),rs,trxName);
-		}
-			
-		return null;
-	}
+    private static final Map<String, ModelDef> MODELS = new HashMap<>();
 
+    static {
+        // Register all your tables + model classes here
+        register(I_ZZ_WF_Line_Role.Table_Name,          MZZWFLineRole.class);
+        register(I_ZZ_WF_Lines.Table_Name,              MZZWFLines.class);
+        register(I_ZZ_WF_Header.Table_Name,             MZZWFHeader.class);
+        register(I_ZZ_Monthly_Levy_Files.Table_Name,    X_ZZ_Monthly_Levy_Files.class);
+        register(I_ZZ_Open_Application.Table_Name,      MZZOpenApplication.class);
+        register(I_ZZ_WSP_ATR_Approvals.Table_Name,     X_ZZ_WSP_ATR_Approvals.class);
+        register(I_ZZ_Program_Master_Data.Table_Name,   MZZProgramMasterData.class);
+        register(I_ZZ_System_Access_Application.Table_Name, MZZSystemAccessApplication.class);
+        register(I_M_Inventory.Table_Name,              MInventory_New.class);
+        register(I_ZZ_Petty_Cash_Recon_Advance.Table_Name,  MZZPettyCashReconAdvance.class);
+        register(I_ZZ_Petty_Cash_Recon_Claim.Table_Name,    MZZPettyCashReconClaim.class);
+        register(I_ZZ_Petty_Cash_Recon_Hdr.Table_Name,      MZZPettyCashReconHdr.class);
+        register(I_ZZ_Petty_Cash_Claim_Line.Table_Name,     MZZPettyCashClaimLine.class);
+        register(I_ZZ_Petty_Cash_Claim_Hdr.Table_Name,      MZZPettyCashClaimHdr.class);
+        register(I_ZZ_Petty_Cash_Advance_Line.Table_Name,   MZZPettyCashAdvanceLine.class);
+        register(I_ZZ_Petty_Cash_Advance_Hdr.Table_Name,    MZZPettyCashAdvanceHdr.class);
+        register(I_ZZ_Petty_Cash_Application.Table_Name,    MZZPettyCashApplication.class);
+        register(I_C_InvoiceBatch.Table_Name,           MInvoiceBatch_New.class);
+        register(I_C_BP_BankAccount.Table_Name,         MBPBankAccount_New.class);
+    }
+
+    private static void register(String tableName, Class<? extends PO> modelClass) {
+        try {
+            Constructor<? extends PO> idCtor =
+                    modelClass.getConstructor(Properties.class, int.class, String.class);
+            Constructor<? extends PO> rsCtor =
+                    modelClass.getConstructor(Properties.class, ResultSet.class, String.class);
+
+            MODELS.put(tableName, new ModelDef(modelClass, idCtor, rsCtor));
+        } catch (NoSuchMethodException e) {
+            // Fail fast at startup if the model doesn't have the standard constructors
+            throw new ExceptionInInitializerError(
+                    "Invalid PO class " + modelClass.getName() +
+                    " for table " + tableName +
+                    " (expected (Properties,int,String) and (Properties,ResultSet,String) constructors)"
+            );
+        }
+    }
+
+    @Override
+    public Class<?> getClass(String tableName) {
+        ModelDef def = MODELS.get(tableName);
+        return def != null ? def.modelClass : null;
+    }
+
+    @Override
+    public PO getPO(String tableName, int Record_ID, String trxName) {
+        ModelDef def = MODELS.get(tableName);
+        if (def == null) {
+            return null;
+        }
+        try {
+            return def.idCtor.newInstance(Env.getCtx(), Record_ID, trxName);
+        } catch (Exception e) {
+            throw new AdempiereException(
+                    "Error creating PO (id) for table " + tableName + " and record " + Record_ID, e);
+        }
+    }
+
+    @Override
+    public PO getPO(String tableName, ResultSet rs, String trxName) {
+        ModelDef def = MODELS.get(tableName);
+        if (def == null) {
+            return null;
+        }
+        try {
+            return def.rsCtor.newInstance(Env.getCtx(), rs, trxName);
+        } catch (Exception e) {
+            throw new AdempiereException(
+                    "Error creating PO (ResultSet) for table " + tableName, e);
+        }
+    }
 }
